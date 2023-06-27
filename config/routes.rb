d3553cc807fbf60c8a25390ac80c468db837b91d
shin <megushin0715@gmail.com>
2023-06-27 22:55:54 +0900

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'users#new'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 end
